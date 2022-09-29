721346a42f9fb0c4236e4ccde711741d85792d78
dlculver <dominic.culver@gmail.com>
2022-09-29 13:35:33 +0200

    added data.py and node.py

---
+++ data.py
@@ -0,0 +1,4 @@
+types = ['ethics','metaphyics','aesthetics','ontology', 'logic', 'epistemology','political philosophy', 'philosophy of mind','philosophy of science','philosophy of mathematics', 'continental philosophy', 'analytic philosophy', 'ancient philosphy', 'ancient greek and roman philosophy', 'Medieval philosophy', 'Modern and contemporary philosophy', 'western philosophy']
+
+
+## data should consist of title, author, publisher, publication date, some of these may be empty, especially for old stuff or we use modern translations
