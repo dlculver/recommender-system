0f5826b1e05769fbb20372db218901bb93834213
dlculver <dominic.culver@gmail.com>
2022-10-04 20:18:11 +0200

    added an override for the string method in node.py to correct error where the stringify_list method was giving location of the nodes rather than their values

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import csv
 
 types = ['ethics','metaphyics','aesthetics', 'epistemology','political philosophy','philosophy of science','philosophy of mathematics', 'philosophy of language', 'existentialism']
 types.sort()
-def insert_phil_types():
-    phil_types_ll = LinkedList()
-    for type in types:
-        current_node = Node(type)
-        phil_types_ll.insert_beginning(current_node)
-    return phil_types_ll
+
+
+phil_types_ll = LinkedList()
+for type in types:
+    current_node = Node(type)
+    phil_types_ll.insert_beginning(current_node)
 
 ## we want to create a linked list of linked lists, there will be a node for each type
 ## and the value of the node will be a linked list of all the works that fall under that types
@@ -31,24 +31,24 @@ for n in range(len(phil_books)):
 ## creating linked list of linked lists, with the each node being a linked list consisting of all books of that particular type
 
 
-def insert_book_data():
-    phil_book_data_ll = LinkedList()
-    for phil_type in types:
-        phil_type_sublist = LinkedList()
-        for book in phil_books:
-            if book[0] == phil_type:
-                phil_type_sublist.insert_beginning(book)
-        phil_book_data_ll.insert_beginning(phil_type_sublist)
-    return phil_book_data_ll
 
-my_phil_types = insert_phil_types()
-my_phil_books = insert_book_data()
+phil_book_data_ll = LinkedList()
+for phil_type in types:
+    phil_type_sublist = LinkedList()
+    for book in phil_books:
+        if book[0] == phil_type:
+            phil_type_sublist.insert_beginning(book)
+    phil_book_data_ll.insert_beginning(phil_type_sublist)
+
+
+# my_phil_types = insert_phil_types()
+# my_phil_books = insert_book_data()
 
 ## welcome message for user
 
 def welcome():
     print("Welcome to the philosophy recommender system! \nA tool for finding philosophy books to read by type :)")
-    see_types = str(input("Would you like to see a list of the types we have data on? \n[y/n]"))
+    see_types = str(input("Would you like to see a list of the types we have data on? \n[y/n]: "))
     valid_responses = ['y','n']
     while see_types not in valid_responses:
         str(input("Please enter [y/n]: "))
@@ -58,16 +58,48 @@ def welcome():
     if see_types == 'n':
         pass
 
+# print("trying to see if we are getting values of nodes")
+#
+# print(phil_types_ll.stringify_list())
+
+# current_node = phil_types_ll.get_head_node()
+# while current_node is not None:
+#     print(str(current_node.get_value()))
+#     current_node = current_node.get_next_node()
+
+#
 welcome()
 
 ## now we will elicit input from the user
 
 selected_phil_type = ''
-#
-# while len(selected_phil_type) == 0:
-#     usr_input = str(
-#         input(
-#             "\nWhat kind of philosophy book would you like to read? \nWould you like to see a list of the types of philosophy to see if its here?.\n"
-#         )
-#     )
+
+## code for interaction with user here
+
+while len(selected_phil_type) == 0:
+    usr_input = str(
+        input(
+            "\nWhat kind of philosophy book would you like to read? \nType the beginning of that type of philosophy to see if it is here.\n"
+        )
+    )
+
+    ## search for user_input in philosophy types data structure
+
+    matching_list = []
+    current_node = phil_types_ll.get_head_node()
+    while current_node is not None:
+        if str(current_node).startswith(usr_input):
+            matching_list.append(str(current_node))
+        current_node = current_node.get_next_node()
+
+    print(matching_list)
+
+
+    # ## print types of matching philosophy
+    #
+    # for phil_type in matching_list:
+    #     print(phil_type)
+
+
+
 
--- node.py
+++ node.py
@@ -1,15 +1,17 @@
-## root file recommender-system.py
-
 class Node:
-    def __init__(self, value, next_node = None):
+    def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
 
-    def set_next_node(self, next_node):
-        self.next_node = next_node
+    def get_value(self):
+        return self.value
 
     def get_next_node(self):
         return self.next_node
 
-    def get_value(self):
-        return self.value
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+## we need to override str so that we can use the command stringify_list in linked list
+    ## otherwise it will only return the location of the nodes, and not their values
+    def __str__(self):
+        return str(self.value)
